5e966331bd885e9cc26c262f37885398ef687ea8
Diana <diana1310fg@gmail.com>
2023-02-20 18:38:57 -0600

    Tipos básicos y conceptos generales.

---
+++ ts-intro/src/ejercicios/01-tipos-basicos.ts
@@ -0,0 +1,14 @@
+/*
+    ===== Código de TypeScript =====
+*/
+
+//let nombre = "Diana";
+let nombre : string = "Diana";
+let hp : number | string = 95;
+let estaVivo: boolean = true;
+
+nombre = 'Jesús';
+hp = "FULL";
+
+
+console.log(nombre, hp, estaVivo);
