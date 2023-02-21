bd02dc112840bd6f3ea3008e95656fc5e6f83209
Diana <diana1310fg@gmail.com>
2023-02-21 12:22:30 -0600

    Extender una clase.

---
+++ ts-intro/src/ejercicios/08-clases.ts
@@ -0,0 +1,31 @@
+/*
+    ===== Código de TypeScript =====
+*/
+
+class PersonaNormal {
+    constructor (
+        public nombre: string,
+        public direccion: string
+    ) { }
+}
+
+class Heroe extends PersonaNormal {
+    // alterEgo: string;
+    // edad: number;
+    // nombreReal: string;
+
+    constructor (
+        public alterEgo: string,
+        public edad: number,
+        public nombreReal: string
+    ) {
+        super(nombreReal, 'New York, USA');
+    }
+
+}
+
+const ironMan = new Heroe('Iron Man', 45, 'Tony Stark');
+
+console.log(ironMan);
+
+
--- ts-intro/src/index.ts
+++ ts-intro/src/index.ts
@@ -2,21 +2,5 @@
     ===== Código de TypeScript =====
 */
 
-class Heroe {
-    // alterEgo: string;
-    // edad: number;
-    // nombreReal: string;
-
-    constructor (
-        public alterEgo: string,
-        public edad: number,
-        public nombreReal: string
-    ) { }
-
-}
-
-const ironMan = new Heroe('Iron Man', 45, 'Tony Stark');
-
-console.log(ironMan);
 
 
