34b54ef4886c628dcc63b636407be44072f9f74a
Diana <diana1310fg@gmail.com>
2023-02-21 11:48:44 -0600

    Clases básicas.

--- ts-intro/src/index.ts
+++ ts-intro/src/index.ts
@@ -2,3 +2,18 @@
     ===== Código de TypeScript =====
 */
 
+class Heroe {
+    alterEgo: string;
+    edad: number;
+    nombreReal: string;
+    
+    imprimirNombre () {
+        return this.alterEgo + ' ' + this.nombreReal;
+    }
+}
+
+const ironMan = new Heroe();
+
+console.log(ironMan);
+
+
