135df8cbf3e079f3d4aca901958086743a3c1b74
Diana <diana1310fg@gmail.com>
2023-02-21 22:17:32 -0600

    Métodos en el Componente.

--- Angular/01-bases/src/app/app.component.ts
+++ Angular/01-bases/src/app/app.component.ts
@@ -5,5 +5,11 @@ import { Component } from '@angular/core';
   templateUrl: './app.component.html'
 })
 export class AppComponent {
-  title = 'Contador App';
+  titulo: string = 'Contador App';
+  numero: number = 10;
+
+  acumular (valor: number) {
+    this.numero += valor;
+  }
+
 }
