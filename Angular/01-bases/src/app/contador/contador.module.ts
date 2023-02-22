b9f9182840efc871f8307b7493d1ffb445b067df
Diana <diana1310fg@gmail.com>
2023-02-22 17:50:04 -0600

    Módulos.

--- Angular/01-bases/src/app/contador/contador.module.ts
+++ Angular/01-bases/src/app/contador/contador.module.ts
@@ -4,17 +4,12 @@ import { NgModule } from '@angular/core';
 import { ContadorComponent } from './contador/contador.component';
 
 @NgModule({
-
     declarations: [
         ContadorComponent
     ],
     exports: [
         ContadorComponent
-    ],
-    imports: [
-        CommonModule
     ]
-
 })
 
 export class ContadorModule {}
