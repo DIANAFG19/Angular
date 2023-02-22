5049cbd8d78cfd71e7b01ba1e1f1afadbef98021
Diana <diana1310fg@gmail.com>
2023-02-22 14:05:49 -0600

    Directiva *ngIf.

--- Angular/01-bases/src/app/heroes/listado/listado.component.ts
+++ Angular/01-bases/src/app/heroes/listado/listado.component.ts
@@ -8,17 +8,19 @@ export class ListadoComponent {
 
   heroes: string[] = ['Iron Man', 'Capitan America', 'Hulk', 'Mujer Maravilla', 'Ant Man', 'Thor'];
   heroesBorrados: string[] = [];
+  apareceBorrado: boolean = false;
 
   borrarHeroe () {
     console.log("Borrando héroe...");
-    let borrado = this.heroes.shift() || '';
-    this.insertarHeroeBorrado(borrado);
+    let borrado = this.heroes.shift();
+    if (borrado != null || borrado != undefined){
+      this.insertarHeroeBorrado(borrado);
+    }
   }
 
   insertarHeroeBorrado (borrado: string) {
+    this.apareceBorrado = true;
     this.heroesBorrados.push(borrado);
   }
-
-  
 
 }
