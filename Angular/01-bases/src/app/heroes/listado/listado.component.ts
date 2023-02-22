3622a91eecf3425005f466ed6c7e8b63a0efd240
Diana <diana1310fg@gmail.com>
2023-02-22 13:45:03 -0600

    Directiva *ngFor.

--- Angular/01-bases/src/app/heroes/listado/listado.component.ts
+++ Angular/01-bases/src/app/heroes/listado/listado.component.ts
@@ -6,4 +6,19 @@ import { Component } from '@angular/core';
 })
 export class ListadoComponent {
 
+  heroes: string[] = ['Iron Man', 'Capitan America', 'Hulk', 'Mujer Maravilla', 'Ant Man', 'Thor'];
+  heroesBorrados: string[] = [];
+
+  borrarHeroe () {
+    console.log("Borrando héroe...");
+    let borrado = this.heroes.shift() || '';
+    this.insertarHeroeBorrado(borrado);
+  }
+
+  insertarHeroeBorrado (borrado: string) {
+    this.heroesBorrados.push(borrado);
+  }
+
+  
+
 }
