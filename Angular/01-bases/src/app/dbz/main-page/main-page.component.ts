7482924ea688b9a7e677504a4d27fc8c91d718dc
Diana <diana1310fg@gmail.com>
2023-02-22 19:55:06 -0600

    NgModel.

--- Angular/01-bases/src/app/dbz/main-page/main-page.component.ts
+++ Angular/01-bases/src/app/dbz/main-page/main-page.component.ts
@@ -1,14 +1,28 @@
 import { Component } from '@angular/core';
 
+interface Personaje {
+  nombre: string;
+  poder: number;
+}
+
 @Component({
   selector: 'app-main-page',
   templateUrl: './main-page.component.html'
 })
 
 export class MainPageComponent {
-  
-  agregar () {
-    console.log("Esta e suna pruebna");
+
+  nuevo: Personaje = {
+    nombre: 'Trunks',
+    poder: 14000
   }
   
+  agregar () {
+    console.log(this.nuevo);
+  }
+
+  cambiarNombre (event: any) {
+    console.log(event.target.value);
+  }
+
 }
