0de19541832c5585258b226bee1eb2d615839bd6
Diana <diana1310fg@gmail.com>
2023-03-03 12:20:21 -0600

    Controlar el historial de búsquedas.

--- Angular/02-gifsApp/src/app/gifs/services/gifs.service.ts
+++ Angular/02-gifsApp/src/app/gifs/services/gifs.service.ts
@@ -15,7 +15,15 @@ export class GifsService {
   constructor() { }
 
   buscarGifs(query: string) {
-    this._historial.unshift(query);
+
+    query = query.trim().toLocaleLowerCase();
+
+    if(!this._historial.includes(query)) {
+      this._historial.unshift(query);
+      this._historial = this._historial.splice(0, 10);
+    }
+    
     console.log(this._historial);
   }
+
 }
