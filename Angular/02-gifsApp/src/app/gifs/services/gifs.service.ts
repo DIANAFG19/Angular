2d9701d549994753e7cbcc850bc065caa05ad4a9
Diana <diana1310fg@gmail.com>
2023-03-03 13:05:47 -0600

    Realizar una petición HTTP.

--- Angular/02-gifsApp/src/app/gifs/services/gifs.service.ts
+++ Angular/02-gifsApp/src/app/gifs/services/gifs.service.ts
@@ -1,3 +1,4 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 
 @Injectable({
@@ -6,13 +7,15 @@ import { Injectable } from '@angular/core';
 
 export class GifsService {
 
+  private apiKey: string = 'q4S1vpwgkg67zXGmv42XxK1tv0Ih8Yag';
+
   private _historial: string[] = [];
 
   get historial () {
     return [...this._historial];
   }
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
 
   buscarGifs(query: string) {
 
@@ -22,8 +25,12 @@ export class GifsService {
       this._historial.unshift(query);
       this._historial = this._historial.splice(0, 10);
     }
+
+    this.http.get('https://api.giphy.com/v1/gifs/search?api_key=q4S1vpwgkg67zXGmv42XxK1tv0Ih8Yag&q=one piece&limit=10')
+          .subscribe((resp: any) => {
+            console.log(resp.data);
+          });
     
-    console.log(this._historial);
   }
 
 }
