a7fb468fef5e891faeb0c1a1c7755c31017485bf
Diana <diana1310fg@gmail.com>
2023-03-03 14:04:30 -0600

    LocalStorage.

--- Angular/02-gifsApp/src/app/gifs/services/gifs.service.ts
+++ Angular/02-gifsApp/src/app/gifs/services/gifs.service.ts
@@ -19,7 +19,9 @@ export class GifsService {
     return [...this._historial];
   }
 
-  constructor(private http: HttpClient) { }
+  constructor(private http: HttpClient) { 
+    this._historial = JSON.parse(localStorage.getItem('historial')!) || [];
+  }
 
   buscarGifs(query: string) {
 
@@ -28,6 +30,9 @@ export class GifsService {
     if(!this._historial.includes(query)) {
       this._historial.unshift(query);
       this._historial = this._historial.splice(0, 10);
+
+      localStorage.setItem('historial', JSON.stringify(this._historial));
+
     }
 
     this.http.get<SearchGifsResponse>(`https://api.giphy.com/v1/gifs/search?api_key=${ this.apiKey }&q=${ query }&limit=10`)
