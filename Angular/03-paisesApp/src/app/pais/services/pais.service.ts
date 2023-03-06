81cac84b24380c796bff5a3b75277f9deda09808
Diana <diana1310fg@gmail.com>
2023-03-06 12:13:03 -0600

    Servicio para buscar país.

--- Angular/03-paisesApp/src/app/pais/services/pais.service.ts
+++ Angular/03-paisesApp/src/app/pais/services/pais.service.ts
@@ -7,23 +7,24 @@ import { Country } from '../interfaces/pais.interface';
 @Injectable({
   providedIn: 'root'
 })
+
 export class PaisService {
 
-  private apiUrl: string = 'https://restcountries.eu/rest/v2';
+  private apiUrl: string = 'https://restcountries.com/v2';
 
-  constructor( private http: HttpClient ) { }
+  constructor(private http: HttpClient) { }
 
-  buscarPais( termino: string ): Observable<Country[]> {
+  buscarPais(termino: string): Observable<Country[]> {
     const url = `${ this.apiUrl }/name/${ termino }`;
     return this.http.get<Country[]>( url );
   }
 
-  buscarCapital( termino: string ):Observable<Country[]>{
+  buscarCapital(termino: string):Observable<Country[]>{
     const url = `${ this.apiUrl }/capital/${ termino }`;
     return this.http.get<Country[]>( url );
   }
 
-  getPaisPorAlpha( id: string ):Observable<Country>{
+  getPaisPorAlpha(id: string):Observable<Country>{
     const url = `${ this.apiUrl }/alpha/${ id }`;
     return this.http.get<Country>( url );
   }
