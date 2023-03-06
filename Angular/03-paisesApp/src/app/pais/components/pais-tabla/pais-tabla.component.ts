d882f2ad1bef88504fe4fcf539cd2e8114ab5536
Diana <diana1310fg@gmail.com>
2023-03-06 12:53:25 -0600

    Llenar la tabla de países.

--- Angular/03-paisesApp/src/app/pais/components/pais-tabla/pais-tabla.component.ts
+++ Angular/03-paisesApp/src/app/pais/components/pais-tabla/pais-tabla.component.ts
@@ -1,20 +1,16 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 import { Country } from '../../interfaces/pais.interface';
 
 @Component({
   selector: 'app-pais-tabla',
   templateUrl: './pais-tabla.component.html',
-  styles: [
-  ]
 })
-export class PaisTablaComponent implements OnInit {
+
+export class PaisTablaComponent {
 
   @Input() paises: Country[] =[];
 
   constructor() { }
-
-  ngOnInit(): void {
-  }
 
 }
