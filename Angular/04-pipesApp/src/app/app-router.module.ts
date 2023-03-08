4cf140f8f26b92bc3bc5ca8155461675f9bfd276
Diana <diana1310fg@gmail.com>
2023-03-07 18:33:40 -0600

    Generando rutas de la aplicación.

--- Angular/04-pipesApp/src/app/app-router.module.ts
+++ Angular/04-pipesApp/src/app/app-router.module.ts
@@ -6,7 +6,6 @@ import { NoComunesComponent } from './ventas/pages/no-comunes/no-comunes.compone
 import { NumerosComponent } from './ventas/pages/numeros/numeros.component';
 import { OrdenarComponent } from './ventas/pages/ordenar/ordenar.component';
 
-
 const routes: Routes = [
   {
     path: '',
@@ -29,16 +28,15 @@ const routes: Routes = [
     path: '**',
     redirectTo: ''
   }
-
 ];
-
 
 @NgModule({
   imports: [
-    RouterModule.forRoot( routes )
+    RouterModule.forRoot(routes)
   ],
   exports: [
     RouterModule
   ]
 })
+
 export class AppRouterModule { }
