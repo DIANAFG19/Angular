1da7cb2852a4046f7a7bb19aa28ce2c929907616
Diana <diana1310fg@gmail.com>
2023-03-08 17:02:07 -0600

    Confirmar Component.

--- Angular/05-heroesApp/src/app/heroes/components/confirmar/confirmar.component.ts
+++ Angular/05-heroesApp/src/app/heroes/components/confirmar/confirmar.component.ts
@@ -1,21 +1,16 @@
-import { Component, Inject, OnInit } from '@angular/core';
+import { Component, Inject } from '@angular/core';
 import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
 
 import { Heroe } from '../../interfaces/heroes.interface';
 
 @Component({
   selector: 'app-confirmar',
-  templateUrl: './confirmar.component.html',
-  styles: [
-  ]
+  templateUrl: './confirmar.component.html'
 })
-export class ConfirmarComponent implements OnInit {
+export class ConfirmarComponent {
 
-  constructor( private dialogRef: MatDialogRef<ConfirmarComponent>,
-               @Inject(MAT_DIALOG_DATA) public data: Heroe ) { }
-
-  ngOnInit(): void {
-  }
+  constructor(private dialogRef: MatDialogRef<ConfirmarComponent>,
+               @Inject(MAT_DIALOG_DATA) public data: Heroe) { }
 
   borrar() {
     this.dialogRef.close(true);
