f146ed2ad8df396f8830dc01ea1634901c75d722
Diana <diana1310fg@gmail.com>
2023-03-09 17:12:35 -0600

    Módulo de Productos y ProductosRoutingModule.

--- Angular/11-directivasPersonalizadasApp/src/app/productos/productos-routing.module.ts
+++ Angular/11-directivasPersonalizadasApp/src/app/productos/productos-routing.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+
 import { AgregarComponent } from './pages/agregar/agregar.component';
 
 const routes: Routes = [
