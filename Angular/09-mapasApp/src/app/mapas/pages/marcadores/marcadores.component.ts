cbdb50d62e1220f1c30791f4c4532c5b567fbfdb
Diana <diana1310fg@gmail.com>
2023-03-09 11:06:29 -0600

    Marcadores en MapasApp.

--- Angular/09-mapasApp/src/app/mapas/pages/marcadores/marcadores.component.ts
+++ Angular/09-mapasApp/src/app/mapas/pages/marcadores/marcadores.component.ts
@@ -16,7 +16,6 @@ interface MarcadorColor {
       height: 100%;
       width: 100%; 
     }
-
     .list-group {
       position: fixed;
       top: 20px;
@@ -42,7 +41,7 @@ export class MarcadoresComponent implements AfterViewInit {
   constructor() { }
 
   ngAfterViewInit(): void {
-
+    
     this.mapa = new mapboxgl.Map({
       container: this.divMapa.nativeElement,
       style: 'mapbox://styles/mapbox/streets-v11',
@@ -61,29 +60,22 @@ export class MarcadoresComponent implements AfterViewInit {
 
   }
 
-
   agregarMarcador() {
-
     const color = "#xxxxxx".replace(/x/g, y=>(Math.random()*16|0).toString(16));
-
     const nuevoMarcador = new mapboxgl.Marker({
       draggable: true,
       color
     })
       .setLngLat( this.center )
       .addTo( this.mapa );
-      
     this.marcadores.push({
       color,
       marker: nuevoMarcador
     });
-
     this.guardarMarcadoresLocalStorage()
-
     nuevoMarcador.on('dragend', () => {
       this.guardarMarcadoresLocalStorage();
     });
-
   }
 
   irMarcador( marker: mapboxgl.Marker ) {
@@ -92,59 +84,42 @@ export class MarcadoresComponent implements AfterViewInit {
     });
   }
 
-
   guardarMarcadoresLocalStorage() {
-
     const lngLatArr: MarcadorColor[] = [];
-
     this.marcadores.forEach( m => {
-
       const color = m.color;
       const { lng, lat } = m.marker!.getLngLat();
-
       lngLatArr.push({
         color: color,
         centro: [ lng, lat ]
       });
     })
-
     localStorage.setItem('marcadores', JSON.stringify(lngLatArr) );
-
   }
 
   leerLocalStorage() {
-    
     if ( !localStorage.getItem('marcadores') ) {
       return;
     }
-
     const lngLatArr: MarcadorColor[] = JSON.parse( localStorage.getItem('marcadores')! );
-
     lngLatArr.forEach( m => {
-
       const newMarker = new mapboxgl.Marker({
         color: m.color,
         draggable: true
       })
         .setLngLat( m.centro! )
         .addTo( this.mapa );
-
       this.marcadores.push({
         marker: newMarker,
         color: m.color
       });
-
       newMarker.on('dragend', () => {
         this.guardarMarcadoresLocalStorage();
       });
-
-
     });
-    
   }
 
   borrarMarcador( i: number ) {
-   
     this.marcadores[i].marker?.remove();
     this.marcadores.splice( i, 1);
     this.guardarMarcadoresLocalStorage();
