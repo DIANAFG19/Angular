f94cae6e0aaae7f4beb084063e443ab8e6ec0cc3
Diana <diana1310fg@gmail.com>
2023-03-09 16:43:25 -0600

    Gráfica de dona con petición HTTP a servidor.

--- Angular/10-graficasApp/src/app/graficas/pages/dona-http/dona-http.component.ts
+++ Angular/10-graficasApp/src/app/graficas/pages/dona-http/dona-http.component.ts
@@ -1,22 +1,17 @@
 import { Component, OnInit } from '@angular/core';
 import { ChartType } from 'chart.js';
 import { Color, Label, MultiDataSet } from 'ng2-charts';
+
 import { GraficasService } from '../../services/graficas.service';
 
 @Component({
   selector: 'app-dona-http',
-  templateUrl: './dona-http.component.html',
-  styles: [
-  ]
+  templateUrl: './dona-http.component.html'
 })
 export class DonaHttpComponent implements OnInit {
 
-  public doughnutChartLabels: Label[] = [
-    // 'Download Sales', 'In-Store Sales', 'Mail-Order Sales', 'Other'
-  ];
-  public doughnutChartData: MultiDataSet = [
-    // [350, 450, 100, 150]
-  ];
+  public doughnutChartLabels: Label[] = [];
+  public doughnutChartData: MultiDataSet = [];
   public doughnutChartType: ChartType = 'doughnut';
 
   public colors: Color[] = [
@@ -45,13 +40,11 @@ export class DonaHttpComponent implements OnInit {
     //     this.doughnutChartData.push( values );
 
     //   });
-
+    
     this.graficasService.getUsuariosRedesSocialesDonaData()
       .subscribe( ({ labels, values }) => {
-
         this.doughnutChartLabels = labels;
         this.doughnutChartData.push( values );
-
       })
 
   }
