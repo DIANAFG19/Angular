08be6d69d692597de64b7664e4cd4a09243fe4fd
Diana <diana1310fg@gmail.com>
2023-03-10 14:35:29 -0600

    Corrección de rutas con los módulos de routing.

--- Angular-Avanzado/03-adminpro/src/app/app-routing.module.ts
+++ Angular-Avanzado/03-adminpro/src/app/app-routing.module.ts
@@ -1,27 +1,23 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-// Módulos
+// Módulos.
+import { PagesRoutingModule } from './pages/pages.routing';
 import { AuthRoutingModule } from './auth/auth.routing';
-import { PagesModule } from './pages/pages.module';
 
 import { NopagefoundComponent } from './nopagefound/nopagefound.component';
 
+
 const routes: Routes = [
-  // path: '/dashboard' PagesRouting
-  // path: '/auth' AuthRouting
-  // path: '/medicos' MedicosRouting
-  // path: '/compras' ComprasRouting
-  { path: '', redirectTo: '/dashboard', pathMatch: 'full' },
   { path: '**', component: NopagefoundComponent },
 ];
 
 @NgModule({
   declarations: [],
   imports: [
-    RouterModule.forRoot(routes),
+    RouterModule.forRoot( routes ),
     AuthRoutingModule,
-    PagesModule
+    PagesRoutingModule
   ],
   exports: [ RouterModule ]
 })
