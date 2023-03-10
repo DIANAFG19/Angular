b349d80885cdd95bb8dd30f502c3bdb424b9d4df
Diana <diana1310fg@gmail.com>
2023-03-09 22:27:58 -0600

    Desplegando aplicación y back-end, correción desde el backend Node.

--- Angular/12-auth-server/index.js
+++ Angular/12-auth-server/index.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const cors = require('cors');
+const path = require('path');
+
 const { dbConnection } = require('./db/config');
 
 require('dotenv').config();
@@ -23,7 +25,9 @@ app.use( express.json() );
 app.use( '/api/auth', require('./routes/auth') );
 
 // Manejar demás rutas.
-
+app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'public/index.html'));
+});
 
 app.listen( process.env.PORT, () => {
     console.log(`Servidor corriendo en puerto ${ process.env.PORT }`);
