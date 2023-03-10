1d31d798f65692a10ef999bf9a7922ecdda9a19c
Diana <diana1310fg@gmail.com>
2023-03-09 18:47:17 -0600

    Creéaciónde auth server y configuración de base de datos mongoDB.

--- Angular/12-auth-server/index.js
+++ Angular/12-auth-server/index.js
@@ -1,11 +1,28 @@
 const express = require('express');
+const cors = require('cors');
+const { dbConnection } = require('./db/config');
 
-// Crea el servidor/aplicación de express.
+require('dotenv').config();
+
+// Crear el servidor/aplicación de express
 const app = express();
 
-// Rutas.
-app.use('/api/auth', require('./routes/auth'));
+// Base de datos
+dbConnection();
 
-app.listen(4000, () => {
-    console.log(`Servidor corriendo en puerto ${ 4000 }`);
+// Directorio Público
+app.use( express.static('public') );
+
+// CORS
+app.use( cors() );
+
+// Lectura y parseo del body
+app.use( express.json() );
+
+// Rutas
+app.use( '/api/auth', require('./routes/auth') );
+
+app.listen( process.env.PORT, () => {
+    console.log(`Servidor corriendo en puerto ${ process.env.PORT }`);
 });
+
