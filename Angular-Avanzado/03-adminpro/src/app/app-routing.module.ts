8a4e463ac13a0c5955fe8c62ce64f9aa6b04a983
Diana <diana1310fg@gmail.com>
2023-03-10 14:07:08 -0600

    Creación de Pages Module y Pages Routing Module.

--- Angular-Avanzado/03-adminpro/src/app/app-routing.module.ts
+++ Angular-Avanzado/03-adminpro/src/app/app-routing.module.ts
@@ -3,24 +3,16 @@ import { RouterModule, Routes } from '@angular/router';
 
 // Módulos
 import { AuthRoutingModule } from './auth/auth.routing.module';
+import { PagesModule } from './pages/pages.module';
 
-import { DashboardComponent } from './pages/dashboard/dashboard.component';
-import { ProgressComponent } from './pages/progress/progress.component';
-import { Grafica1Component } from './pages/grafica1/grafica1.component';
 import { NopagefoundComponent } from './nopagefound/nopagefound.component';
-import { PagesComponent } from './pages/pages.component';
 
 const routes: Routes = [
-  { 
-    path: '', 
-    component: PagesComponent,
-    children: [
-      { path: 'dashboard', component: DashboardComponent },
-      { path: 'progress', component: ProgressComponent },
-      { path: 'grafica1', component: Grafica1Component },
-      { path: '', redirectTo: '/dashboard', pathMatch: 'full' },
-    ]
-  },
+  // path: '/dashboard' PagesRouting
+  // path: '/auth' AuthRouting
+  // path: '/medicos' MedicosRouting
+  // path: '/compras' ComprasRouting
+  { path: '', redirectTo: '/dashboard', pathMatch: 'full' },
   { path: '**', component: NopagefoundComponent },
 ];
 
@@ -28,7 +20,8 @@ const routes: Routes = [
   declarations: [],
   imports: [
     RouterModule.forRoot(routes),
-    AuthRoutingModule
+    AuthRoutingModule,
+    PagesModule
   ],
   exports: [ RouterModule ]
 })
--- Angular-Avanzado/03-adminpro/src/app/app.module.ts
+++ Angular-Avanzado/03-adminpro/src/app/app.module.ts
@@ -1,38 +1,31 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+// Módulos
 import { AppRoutingModule } from './app-routing.module';
+import { AuthModule } from './auth/auth.module';
+import { PagesModule } from './pages/pages.module';
 
 import { AppComponent } from './app.component';
 
-// Módulos
-import { AuthModule } from './auth/auth.module';
-
 import { NopagefoundComponent } from './nopagefound/nopagefound.component';
-import { DashboardComponent } from './pages/dashboard/dashboard.component';
 import { BreadcrumbsComponent } from './shared/breadcrumbs/breadcrumbs.component';
 import { SidebarComponent } from './shared/sidebar/sidebar.component';
 import { HeaderComponent } from './shared/header/header.component';
-import { ProgressComponent } from './pages/progress/progress.component';
-import { Grafica1Component } from './pages/grafica1/grafica1.component';
-import { PagesComponent } from './pages/pages.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     NopagefoundComponent,
-    DashboardComponent,
     BreadcrumbsComponent,
     SidebarComponent,
-    HeaderComponent,
-    ProgressComponent,
-    Grafica1Component,
-    PagesComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    AuthModule
+    AuthModule,
+    PagesModule
   ],
   bootstrap: [AppComponent]
 })
