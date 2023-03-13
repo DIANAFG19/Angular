a9400d9db3a6bdc93c2b9ff07e8cbef32c6f770b
Diana <diana1310fg@gmail.com>
2023-03-13 14:44:17 -0600

    Creando el componente de account-settings y su ruta.

--- Angular-Avanzado/03-adminpro/src/app/components/incrementador/incrementador.component.ts
+++ Angular-Avanzado/03-adminpro/src/app/components/incrementador/incrementador.component.ts
@@ -17,28 +17,28 @@ export class IncrementadorComponent implements OnInit {
 
   @Output('valor') valorSalida: EventEmitter<number> = new EventEmitter();
 
-  cambiarValor( valor: number ) {
-    if ( this.progreso >= 100 && valor >= 0 ) {
+  cambiarValor(valor: number) {
+    if (this.progreso >= 100 && valor >= 0) {
       this.valorSalida.emit(100);
       return this.progreso = 100;
     }
-    if ( this.progreso <= 0 && valor < 0 ) {
+    if (this.progreso <= 0 && valor < 0) {
       this.valorSalida.emit(0);
       return this.progreso = 0;
     }
     this.progreso = this.progreso + valor;
-    this.valorSalida.emit( this.progreso );
+    this.valorSalida.emit(this.progreso);
   }
 
-  onChange( nuevoValor: number ){
-    if( nuevoValor >= 100 ) {
+  onChange(nuevoValor: number){
+    if(nuevoValor >= 100) {
       this.progreso = 100;
-    } else if ( nuevoValor <= 0 ) {
+    } else if (nuevoValor <= 0) {
       this.progreso = 0;
     } else {
       this.progreso = nuevoValor;
     }
-    this.valorSalida.emit( this.progreso );
+    this.valorSalida.emit(this.progreso);
   }
 
 }
