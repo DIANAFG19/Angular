40e36eb7db6214635ab6aeba68da9658f8b0e258
Diana <diana1310fg@gmail.com>
2023-03-13 11:33:45 -0600

    Componente de Incrementador.

--- Angular-Avanzado/03-adminpro/src/app/components/incrementador/incrementador.component.ts
+++ Angular-Avanzado/03-adminpro/src/app/components/incrementador/incrementador.component.ts
@@ -15,27 +15,22 @@ export class IncrementadorComponent implements OnInit {
   @Input('valor') progreso: number = 40;
   @Input() btnClass: string = 'btn-primary';
 
-
   @Output('valor') valorSalida: EventEmitter<number> = new EventEmitter();
 
   cambiarValor( valor: number ) {
-
     if ( this.progreso >= 100 && valor >= 0 ) {
       this.valorSalida.emit(100);
       return this.progreso = 100;
     }
-
     if ( this.progreso <= 0 && valor < 0 ) {
       this.valorSalida.emit(0);
       return this.progreso = 0;
     }
-
     this.progreso = this.progreso + valor;
     this.valorSalida.emit( this.progreso );
   }
 
   onChange( nuevoValor: number ){
-    
     if( nuevoValor >= 100 ) {
       this.progreso = 100;
     } else if ( nuevoValor <= 0 ) {
@@ -43,9 +38,6 @@ export class IncrementadorComponent implements OnInit {
     } else {
       this.progreso = nuevoValor;
     }
-
-    
-
     this.valorSalida.emit( this.progreso );
   }
 
