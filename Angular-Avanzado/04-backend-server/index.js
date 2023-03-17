412587f77a591b4707178b2bcb8d2978812651fd
Diana <diana1310fg@gmail.com>
2023-03-17 12:22:38 -0600

    Creación de endpoint de usuarios, modelo, rutas, middlewares, etc...

--- Angular-Avanzado/04-backend-server/index.js
+++ Angular-Avanzado/04-backend-server/index.js
@@ -9,18 +9,17 @@ const { dbConnection } = require('./database/config');
 const app = express();
 
 // Configurar CORS
-app.use( cors() );
+app.use(cors());
+
+// Lectura y parseo del body
+app.use(express.json());
 
 // Base de datos
 dbConnection();
 
 // Rutas
-app.get( '/', (req, res) => {
-    res.json({
-        ok: true,
-        msg: 'Hola Mundo'
-    });
-});
+app.use('/api/usuarios', require('./routes/usuario'));
+//app.use('/api/login', require('./routes/auth'));
 
 app.listen( process.env.PORT, () => {
     console.log('Servidor corriendo en puerto ' + process.env.PORT );
