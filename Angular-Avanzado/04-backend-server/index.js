0f1d02247c4eecd2c2bc46e0f6fc89d02831102c
Diana <diana1310fg@gmail.com>
2023-03-21 11:17:24 -0600

    Creación de Api de Medicos - CRUD.

--- Angular-Avanzado/04-backend-server/index.js
+++ Angular-Avanzado/04-backend-server/index.js
@@ -21,6 +21,7 @@ dbConnection();
 app.use('/api/usuarios', require('./routes/usuario'));
 app.use('/api/login', require('./routes/auth'));
 app.use('/api/hospitales', require('./routes/hospitales'));
+app.use('/api/medicos', require('./routes/medicos'));
 
 app.listen( process.env.PORT, () => {
     console.log('Servidor corriendo en puerto ' + process.env.PORT );
