a47bb2a818980c7e871282c240a801824e34fbbe
Diana <diana1310fg@gmail.com>
2023-04-04 13:40:03 -0600

    Terminando Backend - Config.

--- Angular-Avanzado/04-backend-server/database/config.js
+++ Angular-Avanzado/04-backend-server/database/config.js
@@ -1,25 +1,20 @@
 const mongoose = require('mongoose');
 
-
 const dbConnection = async() => {
-
     try {
-        await mongoose.connect( process.env.DB_CNN , {
+        // Respuesta en caso de generar una respuesta correcta.
+        await mongoose.connect(process.env.DB_CNN , {
             useNewUrlParser: true, 
             useUnifiedTopology: true,
             useCreateIndex: true
         });
-
         console.log('DB Online');
-        
     } catch (error) {
+        // Respuesta en caso de generar una respuesta de error.
         console.log(error);
         throw new Error('Error a la hora de iniciar la BD ver logs');
     }
-
-
 }
-
 
 module.exports = {
     dbConnection
