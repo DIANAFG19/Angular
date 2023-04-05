ee70ebfffc67b63c2b5ae01f48a53fe54e06b8e9
Diana <diana1310fg@gmail.com>
2023-04-05 12:08:51 -0600

    Terminando Frontend - Auth: Login & Register.

--- Angular-Avanzado/03-adminpro/src/app/auth/auth.routing.ts
+++ Angular-Avanzado/03-adminpro/src/app/auth/auth.routing.ts
@@ -5,7 +5,6 @@ import { RegisterComponent } from './register/register.component';
 import { LoginComponent } from './login/login.component';
 
 const routes: Routes = [
-
     { path: 'register', component: RegisterComponent },
     { path: 'login', component: LoginComponent },
 ];
--- Angular-Avanzado/03-adminpro/src/app/auth/login/login.component.ts
+++ Angular-Avanzado/03-adminpro/src/app/auth/login/login.component.ts
@@ -1,8 +1,10 @@
 import { Component, OnInit, NgZone } from '@angular/core';
 import { Router } from '@angular/router';
 import { FormBuilder, Validators } from '@angular/forms';
-import { UsuarioService } from '../../services/usuario.service';
+
 import Swal from 'sweetalert2';
+
+import { UsuarioService } from '../../services/usuario.service';
 
 declare const gapi:any;
 
@@ -17,41 +19,34 @@ export class LoginComponent implements OnInit {
   public auth2: any;
 
   public loginForm = this.fb.group({
-    email: [ localStorage.getItem('email') || '' , [ Validators.required, Validators.email ] ],
-    password: ['', Validators.required ],
+    email: [localStorage.getItem('email') || '' , [Validators.required, Validators.email]],
+    password: ['', Validators.required],
     remember: [false]
   });
 
-
-  constructor( private router: Router,
-               private fb: FormBuilder,
-               private usuarioService: UsuarioService,
-               private ngZone: NgZone ) { }
+  constructor(private router: Router,
+              private fb: FormBuilder,
+              private usuarioService: UsuarioService,
+              private ngZone: NgZone) { }
 
   ngOnInit(): void {
     this.renderButton();
   }
 
-
   login() {
-
-    this.usuarioService.login( this.loginForm.value )
-      .subscribe( resp => {
-
-        if ( this.loginForm.get('remember').value ){ 
-          localStorage.setItem('email', this.loginForm.get('email').value );
+    this.usuarioService.login(this.loginForm.value)
+      .subscribe(resp => {
+        if (this.loginForm.get('remember').value){ 
+          localStorage.setItem('email', this.loginForm.get('email').value);
         } else {
           localStorage.removeItem('email');
         }
-
         // Navegar al Dashboard
         this.router.navigateByUrl('/');
-
       }, (err) => {
         // Si sucede un error
-        Swal.fire('Error', err.error.msg, 'error' );
+        Swal.fire('Error', err.error.msg, 'error');
       });
-
   }
   
   renderButton() {
@@ -62,34 +57,27 @@ export class LoginComponent implements OnInit {
       'longtitle': true,
       'theme': 'dark',
     });
-
     this.startApp();
-
   }
 
-  async startApp() {
-    
+  async startApp() {  
     await this.usuarioService.googleInit();
     this.auth2 = this.usuarioService.auth2;
-
-    this.attachSignin( document.getElementById('my-signin2') );
-    
+    this.attachSignin(document.getElementById('my-signin2'));
   };
 
   attachSignin(element) {
-    
-    this.auth2.attachClickHandler( element, {},
+    this.auth2.attachClickHandler(element, {},
         (googleUser) => {
             const id_token = googleUser.getAuthResponse().id_token;
             // console.log(id_token);
-            this.usuarioService.loginGoogle( id_token )
+            this.usuarioService.loginGoogle(id_token)
               .subscribe( resp => {
                 // Navegar al Dashboard
-                this.ngZone.run( () => {
+                this.ngZone.run(() => {
                   this.router.navigateByUrl('/');
                 })
               });
-
         }, (error) => {
             alert(JSON.stringify(error, undefined, 2));
         });
