7116d304f0fec475901056b02893cee3f374084a
UserCMP <diana1310fg@gmail.com>
2023-04-07 19:02:59 -0600

    App.component.ts & app.module.ts - Proyecto Interceptores.

--- Angular-Avanzado/05-interceptores/src/app/app.component.ts
+++ Angular-Avanzado/05-interceptores/src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+
 import { UsuariosService } from './services/usuarios.service';
 
 @Component({
@@ -8,18 +9,13 @@ import { UsuariosService } from './services/usuarios.service';
 })
 export class AppComponent {
 
-  constructor( private usuariosService: UsuariosService ) {
-
+  constructor(private usuariosService: UsuariosService) {
     this.usuariosService.obtenerUsuarios()
-      .subscribe( resp => {
-
-        console.log( resp );
-
+      .subscribe(resp => {
+        console.log(resp);
       }, (err) => {
         console.log('Error en el appComponent');
       });
-
-
   }
 
 }
--- Angular-Avanzado/05-interceptores/src/app/app.module.ts
+++ Angular-Avanzado/05-interceptores/src/app/app.module.ts
@@ -1,7 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
 import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
+
 import { InterceptorService } from './interceptors/interceptor.service';
 
 import { AppComponent } from './app.component';
