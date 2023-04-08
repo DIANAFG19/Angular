61270e1b6951f75629bb3cd3a4db3a0500a67d8e
UserCMP <diana1310fg@gmail.com>
2023-04-07 18:58:32 -0600

    Servicio de Interceptor - Proyecto Interceptores.

--- Angular-Avanzado/05-interceptores/src/app/interceptors/interceptor.service.ts
+++ Angular-Avanzado/05-interceptores/src/app/interceptors/interceptor.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@angular/core';
 import { HttpInterceptor, HttpRequest, HttpHandler, HttpEvent, HttpHeaders, HttpErrorResponse } from '@angular/common/http';
+
 import { Observable, throwError } from 'rxjs';
 import { catchError } from 'rxjs/operators';
 
@@ -8,29 +9,19 @@ export class InterceptorService implements HttpInterceptor {
 
   constructor() { }
 
-
-
   intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
-
     const headers = new HttpHeaders({
       'token-usuario': 'ABC1290381902ALKSDJ1902'
     });
-
     const reqClone = req.clone({
       headers
     });
-
-
-
-    return next.handle( reqClone ).pipe(
-      catchError( this.manejarError )
+    return next.handle(reqClone).pipe(
+      catchError(this.manejarError)
     );
-
-
   }
 
-
-  manejarError( error: HttpErrorResponse ) {
+  manejarError(error: HttpErrorResponse) {
     console.log('Sucedió un error');
     console.log('Registrado en el log file');
     console.warn(error);
